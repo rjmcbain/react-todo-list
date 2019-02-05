b449da3856c4db03d0a14dda1276ee3302bcd8d9
rjmcbain <rmcbain01@gmail.com>
2019-02-05 11:48:33 -0700

    stash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from "react";
+import Todos from "./components/Todos";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner at 7pm",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
+
+  // Toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo
+  delTodo = id => {
+    console.log(id);
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
