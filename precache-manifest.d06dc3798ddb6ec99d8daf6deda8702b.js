9a385ca8c367c5121a5c33b8873cb9f84a775c54
rjmcbain <rmcbain01@gmail.com>
2019-02-05 14:10:01 -0700

    Updates

---
+++ precache-manifest.d06dc3798ddb6ec99d8daf6deda8702b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69dfd82b46d15cb0d714",
+    "url": "/react-todo-list/static/css/main.193d2478.chunk.css"
+  },
+  {
+    "revision": "69dfd82b46d15cb0d714",
+    "url": "/react-todo-list/static/js/main.69dfd82b.chunk.js"
+  },
+  {
+    "revision": "1dbb4e9e251ff0c5bff1",
+    "url": "/react-todo-list/static/js/1.1dbb4e9e.chunk.js"
+  },
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "84ae77f26fbfabcc7876ee60350fd76b",
+    "url": "/react-todo-list/index.html"
+  }
+];
